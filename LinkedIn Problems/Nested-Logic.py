75a3857bfa377712b6e82622e0a1ba11e8bf1261
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-10-19 11:58:34 +0530

    Create Nested-Logic.py

---
+++ LinkedIn Problems/Nested-Logic.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = raw_input().strip()
+e = raw_input().strip()
+act = a.split(" ")
+exp = e.split(" ")
+
+if int(act[2]) > int(exp[2]):
+    print 10000
+elif int(act[1]) > int(exp[1]):
+    print 500 * (int(act[1]) - int(exp[1]))
+else:
+    print 15 * (int(act[0]) - int(exp[0]))
