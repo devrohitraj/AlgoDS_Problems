72720d326760777157608f0d9e0fda5929666f96
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-10-19 11:59:15 +0530

    Update and rename Nested-Logic.py to Nested_Logic.py

--- LinkedIn Problems/Nested_Logic.py
+++ LinkedIn Problems/Nested_Logic.py
@@ -4,7 +4,9 @@ e = raw_input().strip()
 act = a.split(" ")
 exp = e.split(" ")
 
-if int(act[2]) > int(exp[2]):
+if int(act[2]) <= int(exp[2]) and int(act[1]) <= int(exp[1]) and int(act[0]) <= int(exp[0]):
+    print 0
+elif int(act[2]) > int(exp[2]):
     print 10000
 elif int(act[1]) > int(exp[1]):
     print 500 * (int(act[1]) - int(exp[1]))
